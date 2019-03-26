2c1e9ad95719a9b11f42f5b15579546e03db9872
Likangkang08 <1712340552@qq.com>
2019-03-26 18:25:21 +0800

    secusss

--- common/LoginPage.py
+++ common/LoginPage.py
@@ -1,13 +1,30 @@
-from common import BasePages
+# coding:utf-8
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from common.BasePages import BasePages
+from time import sleep
+
 
 class LoginPage(BasePages):
-    kw = ['id', 'kw']  # 定位搜索输入框
-    su = ['id', 'su']  # 搜索按钮
+    '''
+    这个类主要封装了，登录的find element 和操作事件。
+    '''
 
-    def kw_send(self, value):  # 搜索框输入内容
-        self.send(self.kw, value)
+    # page element identifier
+    username = (By.ID, "kw")
+    password = (By.ID, "su")
 
-    def su_click(self):  # 点击搜索按钮
-        self.click(self.su)
+    # get username textbox and input username
 
 
+    def set_username(self, username):
+        name = self.driver.find_element(*LoginPage.username)
+        name.send_keys(username)
+#        if self.is_element_exist("accountName"):
+ #           print("meiy")
+
+
+    # Get "longin" button and then click
+    def get_button(self):
+        getbtn = self.driver.find_element(*LoginPage.password)
+        getbtn.click()
--- testcase/Test_login.py
+++ testcase/Test_login.py
@@ -1,17 +1,52 @@
+# coding:utf-8
 from selenium import webdriver
-from common import LoginPage
 import unittest
+from common.LoginPage import LoginPage
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+import unittest
+import time
+import common.LoginPage
 
 
-class BaiduSerch(unittest.TestCase):
+class TestLogin(unittest.TestCase):
 
-    def test_baidu(self):
-        dr = webdriver.Firefox()
-        dr.get('https://www.baidu.com')
-        bai = LoginPage(dr)
-        bai.kw_send('selenium')
-        bai.su_click()
+    # Setup
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://www.baidu.com"
+        self.driver.maximize_window()
+
+    # tearDown
+    def tearDown(self):
+        print("用例执行成功")
+        # self.driver.close()
+
+    def test_Login(self):
+        # Step1: open base site
+
+        self.driver.get(self.base_url)
+        # Step2: Open Login page
+        login_page = LoginPage(self.driver)
+        # Step3: Enter username
+
+        login_page.set_username("sysadmin@mirror.com")
+        # Step4: Enter password
+
+        # Checkpoint1: Check popup dialog title
+        # time.sleep(2)
+        # Step5: Cancel dialog
+        time.sleep(5)
+
+        login_page.get_button()
+        time.sleep(20)
 
 
-if __name__ == '__main__':
+# unittest.main()
+
+if __name__ == "__main__":
     unittest.main()
